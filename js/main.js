c91b1838154c9e9e70c66357f09c69559acb0eac
liqf <qfli@qysoft.cn>
2018-03-31 22:06:21 +0800

    修改主题删除之前内容

--- js/main.js
+++ js/main.js
@@ -1,28 +1,33 @@
 $(document).ready(function() {
 
+
   $('a.blog-button').click(function() {
+    // If already in blog, return early without animate overlay panel again.
+    if (location.hash && location.hash == "#blog") return;
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
+    $('.main-post-list').removeClass('hidden');
     currentWidth = $('.panel-cover').width();
-    $('.panel-cover').addClass('animated panel-cover--collapsed slideInLeft');
-    $('.content-wrapper').addClass('animated slideInLeft');
+    if (currentWidth < 2000) {
+      $('.panel-cover').addClass('panel-cover--collapsed');
+    } else {
+      $('.panel-cover').css('max-width',currentWidth);
+      $('.panel-cover').animate({'max-width': '320px', 'width': '22%'}, 400, swing = 'swing', function() {} );
+    }
+
+    
   });
 
   if (window.location.hash && window.location.hash == "#blog") {
     $('.panel-cover').addClass('panel-cover--collapsed');
+    $('.main-post-list').removeClass('hidden');
   }
 
-  if (window.location.pathname != "/") {       // if hexo in subdir of site, should change this line
+  if (window.location.pathname.substring(0, 5) == "/tag/") {
     $('.panel-cover').addClass('panel-cover--collapsed');
   }
 
-  $('.btn-mobile-menu').click(function() {
-    $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
-  });
-
-  $('.navigation-wrapper .blog-button').click(function() {
-    // $('.navigation-wrapper').toggleClass('visible');
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
-  });
-
+  $('.btn-mobile-menu__icon').click(function() {
+    // 导航按钮被点击
+    // this.style.backgroundColor = '#fff'; 设置颜色后会自动消失
+  });  
 });
